a9be59a1d373b55115d75298bb6fbbc012f93599
Mica Kohl <mica.kohl@utexas.edu>
2019-06-25 21:59:36 -0500

    Building in and testing custom color theme

--- personal-site-app/src/app/app.module.ts
+++ personal-site-app/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MyMaterialModule } from './material.modules';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
